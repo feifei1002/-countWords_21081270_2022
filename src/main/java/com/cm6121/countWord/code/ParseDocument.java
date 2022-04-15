51c86250023d64b02fbdb4440892d714c1658502
Fei Liu <liuf29@cardiff.ac.uk>
2022-04-15 21:54:55 +0100

    successfully store title, text and normalized date

--- src/main/java/com/cm6121/countWord/code/ParseDocument.java
+++ src/main/java/com/cm6121/countWord/code/ParseDocument.java
@@ -23,38 +23,14 @@ public class ParseDocument {
     public Document documentParse(List<String> allLines){
         Document document = null;
         String line = allLines.get(1);
-        String[] split = line.split("\"");
-        Document doc = new Document();
-        doc.setTitle(split[0].substring(0,split[0].length()-1).trim());
-        doc.setCreationDate(split[2].substring(1).trim());
-        doc.setText(split[1]);
-        System.out.println(doc.getTitle());
-        System.out.println(doc.getText());
-        System.out.println(doc.getCreationDate());
 
-//        if(allLines.size() > 0) {
-//            for (String s : allLines) {
-//                document = new Document();
-//                String[] split = s.split(parser);
-//
-//                for (String y : split) {
-//                    String[] split1 = y.split(parser);
-//                    System.out.println(split1[0]+".......hello");
-//                    String[] split2 = split1[0].split(",");
-//                    System.out.println(split2[0]+".......bye");
-//                    if (split[0].equals("title")) {
-//                        System.out.println(split1[0].trim()+"ooooo");
-//                        document.setTitle(split1[0].trim());
-//                    } else if (split1[0].equals("text")) {
-//                        System.out.println(split1[1]);
-//                        document.setText(split1[1].trim());
-//                    } else if (split1[0].equals("normalized_date")) {
-//                        System.out.println(split1[1]);
-//                        document.setCreationDate(split1[1].trim());
-//                    }
-//                }
-//            }
-//        }
+        if(allLines.size()>0) {
+            String[] split = line.split("\"");
+            document = new Document();
+            document.setTitle(split[0].substring(0, split[0].length() - 1).trim());
+            document.setCreationDate(split[2].substring(1).trim());
+            document.setText(split[1]);
+        }
         return document;
     }
 }
