43ca264885ac1ead10dde504693d5f0e0617de9e
Fei Liu <liuf29@cardiff.ac.uk>
2022-04-16 15:56:17 +0100

    add more system.out.println

--- src/main/java/com/cm6121/countWord/app/Application.java
+++ src/main/java/com/cm6121/countWord/app/Application.java
@@ -15,27 +15,31 @@ public class Application {
         String documentToRead = ClassLoader.getSystemClassLoader().getResource("FolderDocumentsToRead").getPath();
         System.out.println("The counting words application");
         System.out.println(documentToRead);
+        System.out.println();
 
         File file = new File("C:\\Users\\c21081270\\Desktop\\ASE Year 1\\Java Assessment 2\\cm6121_assessment_2_start\\build\\resources\\main\\FolderDocumentsToRead");
         String[] listFile = file.list();
         System.out.println("The number of documents in the folder is "+listFile.length);
+        System.out.println();
+        ParseDocument parse = new ParseDocument();
         for (String name : listFile) {
-            ParseDocument parse = new ParseDocument();
             List<String> documentLines = parse.readFile("Desktop", "ASE Year 1", "Java Assessment 2", "cm6121_assessment_2_start", "build", "resources", "main", "FolderDocumentsToRead", name);
             Document document = parse.documentParse(documentLines);
             System.out.println("The file name is " + name+", the title is "+document.getTitle()+", the creation date is "+document.getCreationDate());
         }
 
+        System.out.println();
+
         for(int i = 0; i<listFile.length; i++) {
-            ParseDocument pd = new ParseDocument();
-            List<String> documentLines = pd.readFile("Desktop", "ASE Year 1", "Java Assessment 2", "cm6121_assessment_2_start", "build", "resources", "main", "FolderDocumentsToRead", listFile[i]);
-            Document document = pd.documentParse(documentLines);
+            List<String> documentLines = parse.readFile("Desktop", "ASE Year 1", "Java Assessment 2", "cm6121_assessment_2_start", "build", "resources", "main", "FolderDocumentsToRead", listFile[i]);
+            Document document = parse.documentParse(documentLines);
             //System.out.println need to be removed later
+            System.out.println();
             System.out.println("The tile of the document is "+document.getTitle());
             System.out.println("The creation date of the document is "+document.getCreationDate());
-            HashMap<String, Integer> stringIntegerHashMap = pd.readNumberWords(document.getText(), " ");
+            HashMap<String, Integer> stringIntegerHashMap = parse.readNumberWords(document.getText(), " ");
 
-            pd.printWords(stringIntegerHashMap);
+            parse.printWords(stringIntegerHashMap);
         }
 
         CreateFile cf = new CreateFile();
