02c01f26482b20b1ab9fe7ff01fac58f02e00c38
Fei Liu <liuf29@cardiff.ac.uk>
2022-04-26 12:49:30 +0100

    fixed syntax error when writing into the CSV file

--- src/main/java/com/cm6121/countWord/code/WriteDocument.java
+++ src/main/java/com/cm6121/countWord/code/WriteDocument.java
@@ -8,15 +8,15 @@ public class WriteDocument {
     public static void documentWrite(Document document, Map<String, Integer> writeWords, File file){
         try{
             FileOutputStream outputFile = new FileOutputStream(file, false);
-            OutputStreamWriter outputFileWriter = new OutputStreamWriter(outputFile, "UTF-16");
+            OutputStreamWriter outputFileWriter = new OutputStreamWriter(outputFile, "UTF-8");
             BufferedWriter bw = new BufferedWriter(outputFileWriter);
-            bw.write(document.getTitle());
+            bw.write(document.getTitle().trim());
             bw.write(",");
             bw.write(document.getCreationDate());
             bw.newLine();
 
             for(Map.Entry<String, Integer> words: writeWords.entrySet()){
-                bw.write(words.getKey());
+                bw.write(words.getKey().trim());
                 bw.write(",");
                 bw.write(words.getValue().toString());
                 bw.newLine();
